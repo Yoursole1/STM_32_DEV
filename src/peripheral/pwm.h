6de703a6d8691b42fd2b269797c8ee96edf7fcf9
Joshua Beard <joshuabeard@Joshuas-MacBook-Pro-3.local>
2025-11-01 23:10:01 -0700

    rewrote main.c.  Looked over and tested pwm.  TODO test pwm with osc scope

--- src/peripheral/pwm.h
+++ src/peripheral/pwm.h
@@ -34,12 +34,77 @@ typedef struct {
   bool running;       // Whether pin is outputting a PWM signal
 } pwm_pin_t;
 
+
+
+//----------
+// TODO FACT CHECK THESE PINS.  
+// THE VERY FIRST ONE WAS WRONG
+
+// ALSO TODO, INCLUDE AF MODE WITH NUMBERS (maybe pin_info struct or smt idk)
+//----------
+
+
+// TIM1 channels and their corresponding pins
+// #define TIM1_CH1_1 70
+// #define TIM1_CH1_2 119
+// #define TIM1_CH2_1 74
+// #define TIM1_CH2_2 120
+// #define TIM1_CH3_1 76
+// #define TIM1_CH3_2 121
+// #define TIM1_CH4_1 77
+// #define TIM1_CH4_2 122
+
+// TIM2 channels and their corresponding pins
+#define TIM2_CH1_1 37  // PA0
+#define TIM2_CH1_2 51  // PA5
+#define TIM2_CH1_3 138 // PA15
+// #define TIM2_CH2_1 41
+// #define TIM2_CH2_2 161
+// #define TIM2_CH3_1 42
+// #define TIM2_CH3_2 79
+// #define TIM2_CH4_1 47
+// #define TIM2_CH4_2 80
+
+// TIM3 channels and their corresponding pins
+// #define TIM3_CH1_1 52
+// #define TIM3_CH1_2 115
+// #define TIM3_CH1_3 162
+#define TIM3_CH2_1 53  // PA7
+#define TIM3_CH2_2 116 // PC7
+#define TIM3_CH2_3 163 // PB5
+// #define TIM3_CH3_1 56
+// #define TIM3_CH3_2 117
+// #define TIM3_CH4_1 57
+// #define TIM3_CH4_2 118
+
+// TIM4 channels and their corresponding pins
+#define TIM4_CH1_1 100 // PD12
+#define TIM4_CH1_2 164 // PB6
+// #define TIM4_CH2_1 101
+// #define TIM4_CH2_2 165
+// #define TIM4_CH3_1 104
+// #define TIM4_CH3_2 167
+// #define TIM4_CH4_1 105
+// #define TIM4_CH4_2 168
+
+// TIM5 channels and their corresponding pins
+// #define TIM5_CH1_1 40
+// #define TIM5_CH1_2 87
+#define TIM5_CH2_1 41 // PA1
+#define TIM5_CH2_2 88 // PH11
+// #define TIM5_CH3_1 42
+// #define TIM5_CH3_2 89
+// #define TIM5_CH4_1 47
+// #define TIM5_CH4_2 131
+
+
+
 /**
  * @param pin: The integer value of the pin
  * @param frequency: The PWM frequency of the pin
  * @param dutyCycle: The duty cycle of the pwm signal on a scale of 0 to 65535
  */
-void tal_pwm_init(int pin, int frequency, uint16_t dutyCycle, bool *const err);
+void tal_pwm_pin_init(int pin, uint32_t frequency, uint16_t dutyCycle, bool *const err);
 
 /**
  * @param pin: The integer value of the pin
